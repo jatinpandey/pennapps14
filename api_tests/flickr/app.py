770f72e0ce6c7fa68d745d9f845a573810e4ffd1
zhangoose <az177@eden.rutgers.edu>
2014-02-15 12:50:04 -0500

    got flickr image search working

--- api_tests/flickr/app.py
+++ api_tests/flickr/app.py
@@ -1,15 +1,19 @@
 from flask import Flask, render_template, request, url_for
+import sys
+import urllib
+import Image
 import flickr
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-	photos = flickr.photos_search("dog",3)
+	photos = flickr.photos_search(tags="dog",per_page=3)
 	urls = []
 	for photo in photos:
-		urls.append(photo.getURL(size='Square', urlType='source'))
+		urls.append(photo.getURL(size='Medium',urlType='source'))
 
+	print urls
 	return render_template("index.html",urls=urls)
 
 if __name__ == "__main__":
